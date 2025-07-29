f77e949c4263ef549526369ac1b540242195cb1f
j4742352 <j4742352@gmail.com>
2025-07-29 11:13:25 +0200

    Update login.java

--- src/src/login.java
+++ src/src/login.java
@@ -12,32 +12,35 @@ import java.time.Duration;
 
 public class login  {
 	private WebDriver driver;
-    private WebDriverWait wait ;
-    private WebElement  Maintenancetab;
+    private WebDriverWait wait  = new WebDriverWait(driver, Duration.ofSeconds(10));;
     private WebElement profileDropdown;
 	  private WebElement PIMtab;
-		  private WebElement Admintab;
-	  private WebElement Directorytab;
         private WebElement username;
         private WebElement password;
         private WebElement loginBtn;
+        private WebElement Mainenantce;
 	
     @Test
     public <WebDriver, WebDriverWait, WebElement> void LoginFunction(){
 	                System.setProperty("webdriver.chrome.driver", "path/to/chromedriver.exe");
 	   driver = new ChromeDriver();
-	    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		driver.manage().window().maximize();
+        username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        driver.findElement(By.id(String.valueOf(username))).getText();
+        password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
+        driver.findElement(By.id(String.valueOf(password))).getText();
 
-         driver.findElement(By.id(waitForusername())).getText();
-		  driver.findElement(By.id(waitForpassword())).getText();
-        driver.findElement(By.id(waitForloginBtn())).click();
+        loginBtn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='Login']")));
+        driver.findElement(By.id(String.valueOf(loginBtn))).click();
+
 
   try {
 
             System.out.println("Login Test Passed – Dashboard Loaded");
-            driver.findElement(By.id(waitForprofileDropdown())).click();
+      profileDropdown = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("oxd-userdropdown-name")));
+            driver.findElement(By.id(String.valueOf(profileDropdown))).click();
 
             System.out.println("Waiting for 6 seconds on the dashboard...");
             Thread.sleep(6000);
@@ -48,65 +51,29 @@ driver.quit();
         
     }
 
-    public String waitForusername() {
-
-
-        username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("Admin")));
-
-        return username.getText();
-    }
-    public String waitForpassword() {
-        password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("admin123")));
-
-        return password.getText();
-    }
-    public String waitForloginBtn() {
-        loginBtn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='Login']")));
-
-        return loginBtn;
-    }
-    public WebElement waitForprofileDropdown() {
-        profileDropdown = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("oxd-userdropdown-name")));
-
-        return  profileDropdown;
-    }
- public String waitForPIMtab() {
-        PIMtab= wait.until(ExpectedConditions.elementToBeClickable (By.cssSelector("button[type='PIM']")));
-
-        return  PIMtab;
-    }
-	 public WebElement waitForAdmintab() {
-        Admintab = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='Admin']")));
-
-        return  Admintab;
-    }
-	 public WebElement waitForMaintenanceTab() {
-       Maintenancetab = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='Maintenance']")));
-
-        return  Maintenancetab;
-    }
-	 public WebElement waitForDirectorytab() {
-      Directorytab = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='Directory']")));
-
-        return  Directorytab;
-    }
 	
     @Test
     public <WebDriver, WebDriverWait, WebElement> void LoginFunction1(){
  System.setProperty("webdriver.chrome.driver", "path/to/chromedriver.exe");
 	   driver = new ChromeDriver();
-	    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		driver.manage().window().maximize();
-	    
-                driver.findElement(By.id(waitForusername())).getText();
-		  driver.findElement(By.id(waitForpassword())).getText();
-         driver.findElement(By.id(waitForloginBtn())).click();
-        
+
+        username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        driver.findElement(By.id(String.valueOf(username))).getText();
+        password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
+        driver.findElement(By.id(String.valueOf(password))).getText();
+
+        loginBtn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='Login']")));
+        driver.findElement(By.id(String.valueOf(loginBtn))).click();
+
+
         try {
 
             System.out.println("Login Test Passed – Dashboard Loaded");
-            driver.findElement(By.id(waitForPIMtab())).click();
+            PIMtab= wait.until(ExpectedConditions.elementToBeClickable (By.cssSelector("button[type='PIM']")));
+            driver.findElement(By.id(String.valueOf(PIMtab))).click();
 
             System.out.println("Waiting for 6 seconds on the dashboard...");
             Thread.sleep(6000);
@@ -118,3 +85,39 @@ driver.quit();
 
 
     }
+    @Test
+    public <WebDriver, WebDriverWait, WebElement> void LoginFunction2(){
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        driver.manage().window().maximize();
+
+        username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        driver.findElement(By.id(String.valueOf(username))).getText();
+        password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
+        driver.findElement(By.id(String.valueOf(password))).getText();
+
+        loginBtn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='Login']")));
+        driver.findElement(By.id(String.valueOf(loginBtn))).click();
+
+
+        try {
+
+            System.out.println("Login Test Passed – Dashboard Loaded");
+            Mainenantce= wait.until(ExpectedConditions.elementToBeClickable (By.cssSelector("button[type='Mainenantce']")));
+            driver.findElement(By.id(String.valueOf(Mainenantce))).click();
+
+            System.out.println("Waiting for 6 seconds on the dashboard...");
+            Thread.sleep(6000);
+        } catch (Exception e) {
+            System.out.println("Login Test Failed – Dashboard not detected");
+        }
+
+        driver.quit();
+
+
+    }
+}
+
+    
