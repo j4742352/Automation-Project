110da4a24f7fb6cbdd83c27a6c792cc8fe6f8d69
j4742352 <j4742352@gmail.com>
2025-07-29 15:48:34 +0200

    Update login.java

--- src/src/login.java
+++ src/src/login.java
@@ -22,17 +22,17 @@ public class login  {
 	
     @Test
     public <WebDriver, WebDriverWait, WebElement> void LoginFunction(){
-	                 System.setProperty("webdriver.chrome.driver", "path/to/chromedriver.exe");
+	                 System.setProperty("webdriver.chrome.driver", "path/to/chromedriver.exe");//here se.
 	   driver = new ChromeDriver();
 
-		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.get("https://opensource-demo.orangehrmlive.com"); // no access to c1 , c2, c3
 		driver.manage().window().maximize();
-        username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        username = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));// c1
         driver.findElement(By.id(String.valueOf(username))).getText();
-        password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
+        password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));// c2
         driver.findElement(By.id(String.valueOf(password))).getText();
 
-        loginBtn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='Login']")));
+        loginBtn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='Login']")));// c3
         driver.findElement(By.id(String.valueOf(loginBtn))).click();
 
 
