851aad9d20d18cdabf762f2c8341e0907c0ce58d
j4742352 <j4742352@gmail.com>
2025-07-31 09:54:06 +0200

    Update navigation.java

--- src/src/navigation.java
+++ src/src/navigation.java
@@ -19,7 +19,7 @@ public class navigation {
     private WebElement username;
     private WebElement password;
     private WebElement loginBtn;
-    private WebDriver driver;//de
+    private WebDriver driver = new ChromeDriver();//de
     private WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     private WebElement Btn;
     private WebElement timeTab;
